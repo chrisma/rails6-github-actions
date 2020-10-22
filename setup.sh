72e5d8c6f7b4c7df41f96a5295badf46a7b773e8
Christoph Matthies <chrisma@users.noreply.github.com>
2020-10-22 20:53:30 +0200

    Update Gemfile with template

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 # Generate new 'blog' project, skip not needed gems, ignore possible .railsrc file
 rails new blog --database=sqlite3 --skip-webpack-install --skip-spring --skip-listen --no-rc
+
+# Get Gemfile with custom dependencies, overwriting generated Gemfile
+wget https://raw.githubusercontent.com/hpi-swt2/rolodex-portal/preparation/Gemfile -O blog/Gemfile
