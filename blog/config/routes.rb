0a808866f688d541e7b9502482e10b5a5e645a77
Christoph Matthies <chrisma@users.noreply.github.com>
2020-10-23 09:09:11 +0200

    Update routes from Getting Started guide

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # https://guides.rubyonrails.org/getting_started.html#getting-up-and-running
+  resources :articles
+
+  # https://guides.rubyonrails.org/getting_started.html#setting-the-application-home-page
+  root 'welcome#index'
 end
